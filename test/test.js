2c1df4e882bd66018433c24a2cdf217e77b541b8
RayMorgan <rmorgan@zappos.com>
2009-10-08 23:19:30 -0700

    Added main code

---
+++ test/test.js
@@ -0,0 +1,26 @@
+var Vroom = require('../vroom/lib/vroom.js');
+var PathResource = require('../vroom/lib/vroom/path_resource.js');
+
+Vroom.installPlugin(require('vroom-haml.js'));
+
+var resource = new PathResource(function (r) {
+
+  r.get('/', function () {
+    this.foo = "bar";
+    this.haml("test")
+      .addErrback(function (err) {
+        require('/utils.js').puts(err);
+      });
+  });
+
+});
+
+var app = new Vroom.Application();
+
+app.config.use(function (c) {
+  c['logLevel'] = 'DEBUG';
+  c['viewDir'] = node.path.dirname(__filename);
+});
+
+app.mount('root', '/', resource);
+app.boot();
